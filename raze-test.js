c1379c864448b59be2aa0e6dfff4e3260e186e79
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-10 17:23:45 +0800

    Updated test.

--- raze-test.js
+++ raze-test.js
@@ -3,5 +3,7 @@
 const raze = require( "./raze.js" );
 
 console.log( raze( [ 1, 2, 3 ] ) );
+console.log( raze( [ { "name": "simple" } ] ) );
 console.log( raze( [ ] ) );
+console.log( raze( [ { } ] ) );
 console.log( raze( ) );

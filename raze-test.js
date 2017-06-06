fb420e138895d7736ca15bd1918a8970f98082be
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-06 17:36:17 +0800

    Updated test.

--- raze-test.js
+++ raze-test.js
@@ -2,20 +2,40 @@
 const assert = require( "assert" );
 const raze = require( "./raze.js" );
 
-assert.ok( raze( ) );
+assert.deepEqual( raze( ), [ ], "should have empty array" );
 
-assert.ok( raze( null ) );
+assert.deepEqual( raze( null ), [ ], "should have empty array" );
 
-assert.ok( raze( undefined ) );
+assert.deepEqual( raze( undefined ), [ ], "should have empty array" );
 
-assert.ok( raze( false ) );
+assert.deepEqual( raze( NaN ), [ ], "should have empty array" );
 
-assert.ok( raze( true ) );
+assert.deepEqual( raze( Infinity ), [ ], "should have empty array" );
 
-assert.ok( raze( "hello world" ) );
+assert.deepEqual( raze( false ), [ ], "should have empty array" );
 
-assert.ok( raze( [ 1, 2, 3 ] ) );
+assert.deepEqual( raze( true ), [ ], "should have empty array" );
 
-assert.ok( raze( [ ] ) );
+assert.deepEqual( raze( "hello world" ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( 123 ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( [ ] ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( { } ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( { "name": "simple" } ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( Symbol.for( "hello" ) ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( Object ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( Object.protype ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( function hello( ){ } ), [ ], "should have empty array" );
+
+assert.deepEqual( raze( [ 1, 2, 3 ] ), [ 1, 2, 3 ], "should be equal" );
+
+assert.deepEqual( raze( [ { "name": "simple" } ] ), [ { "name": "simple" } ], "should be equal" );
 
 console.log( "ok" );

6624c7b0dff19e2ae81ac3543fd55557a9467e52
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-03 18:44:13 +0800

    Updated dependency.

--- raze.support.js
+++ raze.support.js
@@ -34,10 +34,11 @@
               			"file": "raze.js",
               			"module": "raze",
               			"author": "Richeve S. Bebedor",
-              			"contributors": [
-              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-              			],
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/raze.git",
               			"test": "raze-test.js",
               			"global": true
